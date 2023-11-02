a9550df6919fd72b6353ac371d950d93c0b33036
Matheus <mt@matheustorres.com>
2023-11-02 21:28:46 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.headerMobileMenu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/images/mobile-icon-hamburguer.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "/images/mobile-icon-close.svg";
+    }
+}
