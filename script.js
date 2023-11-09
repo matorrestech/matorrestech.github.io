00393ddd920610e5a0729df556e31391b834bdb7
Matheus <mt@matheustorres.com>
2023-11-09 21:11:11 +0000

    Update - Header and Main - 09.11.23

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function menuShow() {
-    let menuMobile = document.querySelector(".mobile-menu");
-
+    let menuMobile = document.querySelector("header .mobile-menu");
     if (menuMobile.classList.contains("open")) {
         menuMobile.classList.remove("open");
         document.querySelector(".icon").src =
